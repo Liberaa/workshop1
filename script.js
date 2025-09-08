cfd65db0b8f4a8d29e65f75cd593e190a7178319
Kevin Lissov <kl223py@student.lnu.se>
2025-09-08 11:01:27 +0200

    Added multiplication and Division

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
-
-
 let num1 = document.querySelector('#num1')
 let num2 = document.querySelector('#num2')
 let operator = document.querySelector('#operation')
 const result = document.querySelector('#result')
-const result2 = document.querySelector('#result2')
-
 
 function updateSum() {
-    const n1 = Number(num1.value)
-    const n2 = Number(num2.value)
-    result.textContent = "result: " + (n1 + n2)
+  const n1 = Number(num1.value)
+  const n2 = Number(num2.value)
+  const operation = operator.value
+  let output = ''
+
+  if (operation == 'add') {
+    output = n1 + n2
+  } else if (operation == 'sub') {
+    output = n1 - n2
+  } else if (operation == 'mul') {
+    output = n1 * n2
+  } else if (operation == 'div') {
+    if (n2 == 0) {
+      output = 'cannot divide by zero'
+    } else {
+      output = n1 / n2
+    }
+  }
+  result.textContent = 'Result: ' + output
 }
 
-function subSum () {
-     const n1 = Number(num1.value)
-    const n2 = Number(num2.value)
-    result2.textContent = 'res' + (n1 - n2)
-}
 
-num1.addEventListener("input", updateSum)
-num2.addEventListener("input", updateSum)
-
-
-
-
-num1.addEventListener("input", subSum)
-num2.addEventListener("input", subSum)
-
-
-
-
-
+num1.addEventListener('input', updateSum)
+num2.addEventListener('input', updateSum)
